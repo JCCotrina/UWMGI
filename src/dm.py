5988c91737e8924c07803a41f37628b359fb22ca
JCCotrina <jc@OMEN.localdomain>
2022-07-10 23:35:49 -0500

    change of route

--- src/dm.py
+++ src/dm.py
@@ -44,9 +44,9 @@ class Dataset(torch.utils.data.Dataset):
 class DataModule(pl.LightningDataModule):
     def __init__(
         self,
-        base_path='/kaggle/input/uw-madison-gi-tract-image-segmentation',
+        base_path='/data',
         path=os.path.join(base_path, 'train'),
-        file=os.path.join(base_path, 'train.csv')
+        file=os.path.join(base_path, 'data_procesada.csv')
         val_split=0.7, # 120 / 40 / 40
         batch_size=32,
         num_workers=4,
