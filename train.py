e4e35c8a37ec820a25b5dacfd2934a5f4d322ae5
JCCotrina <jc@OMEN.localdomain>
2022-07-15 14:45:17 -0500

    fix error parameter val_split

--- train.py
+++ train.py
@@ -21,23 +21,23 @@ def get_cbs(config):
     return cbs
 
 def train(config):
-    folds = [(1, 32), (33, 64), (65, 96), (97, 128), (129, 160)]
-    for f, fold in enumerate(folds):
-        config['val_split'] = fold
-        dm = DataModule(**config)
-        model = SMP(config)
-        wandb_logger = WandbLogger(project="MnMs2-cv", config=config)
-        trainer = pl.Trainer(
-            gpus=config['gpus'],
-            precision=config['precision'],
-            logger=wandb_logger if config['log'] else None,
-            max_epochs=config['max_epochs'],
-            callbacks=get_cbs(config),
-            limit_train_batches=config['train_batches'],
-            limit_val_batches=config['val_batches']
-        )
-        trainer.fit(model, dm)
-        wandb_logger.experiment.finish()
+    #folds = [(1, 32), (33, 64), (65, 96), (97, 128), (129, 160)]
+    #for f, fold in enumerate(folds):
+    #config['val_split'] = fold
+    dm = DataModule(**config)
+    model = SMP(config)
+    wandb_logger = WandbLogger(project="MnMs2-cv", config=config)
+    trainer = pl.Trainer(
+        gpus=config['gpus'],
+        precision=config['precision'],
+        logger=wandb_logger if config['log'] else None,
+        max_epochs=config['max_epochs'],
+        callbacks=get_cbs(config),
+        limit_train_batches=config['train_batches'],
+        limit_val_batches=config['val_batches']
+    )
+    trainer.fit(model, dm)
+    wandb_logger.experiment.finish()
 
 if __name__ == '__main__':
     config_file = sys.argv[1]
