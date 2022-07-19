76be9059d10294ad157e5283d795cdb203ba4017
JCCotrina <jc@OMEN.localdomain>
2022-07-18 20:56:23 -0500

    depurar getitem de dm.py

--- src/dm.py
+++ src/dm.py
@@ -29,9 +29,11 @@ class Dataset(torch.utils.data.Dataset):
         img = cv2.imread(path_image, cv2.IMREAD_GRAYSCALE).astype('float32')
         norm_image = cv2.normalize(img, None, alpha=0, beta=1,norm_type= cv2.NORM_MINMAX, dtype=cv2.CV_32F)
         if path_mask == '':
+            print("#"*14)
             print(path_mask)
             mask = np.zeros((266, 266)).astype(int)
         else:
+            print("#"*14)
             print(path_mask)
             mask = cv2.imread(path_mask, cv2.IMREAD_UNCHANGED).astype(int)
 
