9b3afbd69002f2b296e23a6f67be208214e84672
JCCotrina <juan.cotrina.m@uni.pe>
2022-07-27 14:36:21 -0500

    debug

--- src/losses/criterion.py
+++ src/losses/criterion.py
@@ -6,4 +6,7 @@ TverskyLoss = smp.losses.TverskyLoss(mode='multilabel', log_loss=False)
 BCELoss     = smp.losses.SoftBCEWithLogitsLoss()
 
 def criterion(pr, gt):
+    print(f'tipo pr:{type(pr)}')
+    print(f'tipo gt:{type(gt)}')
+    
     return 0.5*BCELoss(pr, gt.float()) + 0.5*TverskyLoss(pr, gt)
