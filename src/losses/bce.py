593e2832e2b55f36914603e38bbfd7f7618bf8ec
JCCotrina <juan.cotrina.m@uni.pe>
2022-07-28 01:59:04 -0500

    debug

--- src/losses/bce.py
+++ src/losses/bce.py
@@ -4,4 +4,9 @@ import torch.nn.functional as F
 def bce(pr, gt):
     #input (torch.Tensor): input data tensor with shape :math:`(B, *)`.
     #target (torch.Tensor): the target tensor with shape :math:`(B, *)`.
+    print("\n","#"*12)
+    print(f'input:{pr.shape}')
+    print(f'target:{gt.shape}')
+    print(pr)
+    print(gt)
     return F.binary_cross_entropy_with_logits(pr, gt)
