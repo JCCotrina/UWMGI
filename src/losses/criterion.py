9e67742451b87885bfb53fad38e5f4d49b240220
JCCotrina <juan.cotrina.m@uni.pe>
2022-07-28 17:50:19 -0500

    debug

--- src/losses/criterion.py
+++ src/losses/criterion.py
@@ -4,8 +4,8 @@ import segmentation_models_pytorch as smp
 from .bce import bce 
 
 TverskyLoss = smp.losses.TverskyLoss(mode='multilabel', log_loss=False)
-BCELoss     = smp.losses.SoftBCEWithLogitsLoss()
+# BCELoss     = smp.losses.SoftBCEWithLogitsLoss()
 
 def criterion(pr, gt):
 
-    return F.CrossEntropyLoss(pr, gt)
+    return 0.5*F.BCELoss(pr, gt) + 0.5*TverskyLoss(pr, gt)
