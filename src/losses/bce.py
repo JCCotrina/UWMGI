a7ac1f75920128cfac0bf331dfdac6da95071dfb
JCCotrina <juan.cotrina.m@uni.pe>
2022-07-28 17:03:46 -0500

    debug

--- src/losses/bce.py
+++ src/losses/bce.py
@@ -4,4 +4,4 @@ import torch.nn as F
 def bce(pr, gt):
     #input (torch.Tensor): input data tensor with shape :math:`(B, *)`.
     #target (torch.Tensor): the target tensor with shape :math:`(B, *)`.
-    return F.BCEWithLogitsLoss(pr, gt)
+    return F.BCEWithLogitsLoss()(pr, gt)
