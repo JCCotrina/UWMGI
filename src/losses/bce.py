4ad9c384c8f0e90e48fa65e4245830988d6288f2
JCCotrina <juan.cotrina.m@uni.pe>
2022-07-29 15:52:44 -0500

    debug

--- src/losses/bce.py
+++ src/losses/bce.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as F
 
 def bce(pr, gt):
+    loss = F.CrossEntropyLoss()
     #input (torch.Tensor): input data tensor with shape :math:`(B, *)`.
     #target (torch.Tensor): the target tensor with shape :math:`(B, *)`.
-    return F.BCEWithLogitsLoss()(pr, gt)
+    return loss(pr, gt)
