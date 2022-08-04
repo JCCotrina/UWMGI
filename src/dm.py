2520bdb865ab86ea1a7d1aff26413c26bc94cd90
JCCotrina <juan.cotrina.m@uni.pe>
2022-08-03 21:04:02 -0500

    unet con efficeentment 2 en yml

--- src/dm.py
+++ src/dm.py
@@ -94,15 +94,12 @@ class DataModule(pl.LightningDataModule):
                     A.Resize( height=224, width=224, interpolation=cv2.INTER_NEAREST),
                     A.HorizontalFlip(p=0.5),
                     A.VerticalFlip(p=0.5),
-                    A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.05, rotate_limit=10, p=0.5),
+                    A.Transpose( p=0.5),
                     A.OneOf([
                         A.GridDistortion(num_steps=5, distort_limit=0.05, p=1.0),
             # #             A.OpticalDistortion(distort_limit=0.05, shift_limit=0.05, p=1.0),
-                        A.ElasticTransform(alpha=1, sigma=50, alpha_affine=50, p=1.0),
-                        A.GaussianBlur(p=1.0),
+                        A.ElasticTransform(alpha=1, sigma=224*0.05, alpha_affine=224*0.05, p=1.0),
                     ], p=0.25),
-                    A.CoarseDropout(max_holes=8, max_height=11, max_width=11,
-                                    min_holes=5, fill_value=0, mask_fill_value=0, p=0.5),
                     ], p=1.0),
         )
 
